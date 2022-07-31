0de80b4864928c24d69d6412ece06b98639d3460
jp <jeanphilippe292020@gmail.com>
2022-07-30 21:11:55 -0700

    main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,31 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example-example" {
+resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.SG-name.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
-    Name = "terraform-example"
+    Name = "terraform-example1"
   }
+}
 
+resource "aws_security_group" "SG-name" {
+  name = "terraform-example-instance"
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
